dec4bd5a44e2e99b1e8aa81231b174a721066560
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2020-04-27 11:54:15 +0700

    Update moving notice

--- Standalone/ThaiLotteryChecker.py
+++ Standalone/ThaiLotteryChecker.py
@@ -1,5 +1,5 @@
 ### Moving notice ###
 This script is moved to Github Gist (https://gist.github.com/NP-chaonay/a94dae7ee8016fa63d32db346688a337).
 Sorry for any inconvenience. The reason is I've seen the place to host the standalone script/program file at Github Gist.
-This file will be delete from the repository after 1 month of this announcement.
+This notice file will be delete from the repository after 1 month of this announcement.
 - NP-chaonay at 2020-04-27 04:51 (UTC)
